9e843463c8498b8346195c82e0b3b8ee5c70e9c6
Dimaer77 <dimaer77@gmail.com>
2021-12-02 17:48:17 +0300

    add Add Card Flip

--- src/n2-features/gamePlay/PlayGround.tsx
+++ src/n2-features/gamePlay/PlayGround.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,6 +9,7 @@ import { CardType, initCardsStateType } from '../f1-table/cards/CardsReducer';
 
 import { SetPlayCardThunk } from './Bll/PlayCardReducer';
 import { pseudoRandom } from './function/pseudoRandom';
+import style from './playGround.module.scss';
 
 export const PlayGround: FC = (): React.ReactElement => {
   const dispatch = useDispatch();
@@ -17,39 +18,109 @@ export const PlayGround: FC = (): React.ReactElement => {
     AppRootStateType,
     initCardsStateType
   >(state => state.cards);
+
   useEffect(() => {
     dispatch(SetPlayCardThunk(_idPackCards!, cardsTotalCount!));
-  }, []);
+  }, [_idPackCards]);
+  const [flipCard, setFlipCard] = useState<boolean>(false);
   const card = pseudoRandom(
     useSelector<AppRootStateType, Array<CardType> | null>(state => state.playCard.cards),
   );
   if (!card) {
     return <Loader />;
   }
+
   const render = loading ? (
     <Loader />
   ) : (
-    <div>
-      <h1>{card.question}</h1>
-      <h2>{card.answer}</h2>
-      <div>
-        <span>raiting : {card.rating}</span>
+    <div className={style.cardShowContainer}>
+      <div className={style.flipBox}>
+        <div
+          className={
+            flipCard ? `${style.cardFront} ${style.cardFrontFlip}` : style.cardFront
+          }
+        >
+          <p className={style.titleQuestion}>{card.question}</p>
+
+          <div className={style.ratingGradeWrap}>
+            <span className={style.cardRating}>Rating Card : {card.rating}</span>
+            <span className={style.cardGrade}>Grade Card : {card.grade}</span>
+            <div className={style.ratingButContainer}>
+              <button
+                onClick={() => console.log(1)}
+                type="button"
+                className="btn btn-warning"
+              >
+                1
+              </button>
+              <button
+                onClick={() => console.log(2)}
+                type="button"
+                className="btn btn-warning"
+              >
+                2
+              </button>
+              <button
+                onClick={() => console.log(3)}
+                type="button"
+                className="btn btn-warning"
+              >
+                3
+              </button>
+              <button
+                onClick={() => console.log(4)}
+                type="button"
+                className="btn btn-warning"
+              >
+                4
+              </button>
+              <button
+                onClick={() => console.log(5)}
+                type="button"
+                className="btn btn-warning"
+              >
+                5
+              </button>
+            </div>
+          </div>
+
+          <div className={style.butContainer}>
+            <Button variant="secondary" onClick={() => window.history.back()}>
+              Exit game
+            </Button>
+
+            <button
+              onClick={() => setFlipCard(true)}
+              type="button"
+              className="btn btn-warning"
+            >
+              Show Answer
+            </button>
+            <Button
+              variant="secondary"
+              onClick={() => {
+                dispatch(SetPlayCardThunk(_idPackCards!, cardsTotalCount!));
+              }}
+            >
+              Next Card
+            </Button>
+          </div>
+        </div>
+        <div
+          className={
+            flipCard ? style.cardBack : `${style.cardBack} ${style.cardBackFlip}`
+          }
+        >
+          <h2>{card.answer}</h2>
+          <button
+            onClick={() => setFlipCard(false)}
+            type="button"
+            className="btn btn-warning"
+          >
+            Back to the question
+          </button>
+        </div>
       </div>
-      <div>
-        <span>grade : {card.grade}</span>
-      </div>
-      <div>оценочка так сказать</div>
-      <Button variant="secondary" onClick={() => window.history.back()}>
-        Back
-      </Button>
-      <Button
-        variant="secondary"
-        onClick={() => {
-          dispatch(SetPlayCardThunk(_idPackCards!, cardsTotalCount!));
-        }}
-      >
-        Next
-      </Button>
     </div>
   );
   return render;
